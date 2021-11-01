0975bb160f623a609601257e284544b7d4d5a5b1
Jimmy Boinembalome <jboinembalome@gmail.com>
2021-11-01 13:43:18 +0100

    Add card component to display new

---
+++ src/app/shared/components/article-card/article-card.component.ts
@@ -0,0 +1,18 @@
+import { Component, Input } from '@angular/core';
+
+@Component({
+    selector     : 'tw-article-card',
+    templateUrl  : './article-card.component.html',
+})
+export class ArticleCardComponent {
+    @Input() sourceName?: string = '';
+    @Input() url?: string = '';
+    @Input() urlToImage?: string = '';
+    @Input() title?: string = '';
+    @Input() description?: string = '';
+    @Input() author?: string = '';
+    @Input() publishedAt?: Date | null = null;
+    
+    constructor() {
+    }
+}
